f998863433296e31bd8b1f6e0b5d30859372715f
Pol Reig <polreigbroto@gmail.com>
2021-06-10 17:41:04 +0200

    componento Circulo añadido, evento click y useState utilizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+/* import { useState } from "react"; */
+import { Circulo } from "./Components/Circulo";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [estado, setEstado] = useState(true);
+  const circulos = [1, 2, 3, 4, 5];
+const cambiarEstado = () => {
+  setEstado(!estado)
+
+  return(
+  <>
+  <ul> {circulos.map((circulo) => (
+  <Circulo key={circulo} />))}
+  </ul>
+</>
+  )}
 
 export default App;
---
+++ src/Components/Circulo.js
@@ -0,0 +1,10 @@
+export const Circulo = (props) => {
+  const {  cambiarEstado } = props;
+  return (
+        <li className="Circle" onClick={cambiarEstado}>
+
+        </li>
+      ))}
+
+  );
+};
